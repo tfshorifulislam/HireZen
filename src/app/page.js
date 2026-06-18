554d23de5778ed184483e7264ee3581a79439c5d
tfshorifulislam <tfshorifulislam@gmail.com>
2026-06-18 23:32:44 +0600

    protected route create

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import Automatejob from '@/components/PublicComponents/Automatejob';
 import Banner from '@/components/PublicComponents/Banner';
 import FeaturingJobs from '@/components/PublicComponents/FeaturingJobs';
-import PublicFooter from '@/components/PublicComponents/Footer';
 import HiringCompanies from '@/components/PublicComponents/HiringCompanies';
 import HowDifferentHireZen from '@/components/PublicComponents/HowDifferentHireZen';
 import JobSeekerBanner from '@/components/PublicComponents/JobSeekerBanner';
-import LoginSignupButton from '@/components/PublicComponents/LoginSignupButton';
-import PublicNavbar from '@/components/PublicComponents/Navbar';
+
 import React from 'react';
 
 const PublicHomePage = () => {
